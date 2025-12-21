a76f14d720e84c20b318da3859412c8c523566ff
希亚 <shia@dice.zone>
2025-12-22 02:03:01 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -269,7 +269,7 @@ export default function HomePage() {
         <div className="header">
           <h1 className="title">SealDice Log Service</h1>
           <div style={{ textAlign: 'center' }}>
-            <span className="version-badge">v20251119-beta0437</span>
+            <span className="version-badge">v20251222-beta0202</span>
           </div>
         </div>
         <p className="description">
